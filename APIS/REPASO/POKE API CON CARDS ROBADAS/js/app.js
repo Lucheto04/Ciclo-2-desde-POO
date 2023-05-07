fba61f5c77f665cabe431833004b8a63b4d42b8c
ErG <emanuel.erg91@gmail.com>
2023-05-06 20:00:17 -0500

    POKE API RODABA

---
+++ APIS/REPASO/POKE API CON CARDS ROBADAS/js/app.js
@@ -0,0 +1,21 @@
+function randomElement(max) {
+    return Math.floor(Math.random() * max);
+}
+
+let pokeRandom = randomElement(150);
+console.log(randomElement(pokeRandom));
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+});
+
+const fetchData = async () => {
+    try {
+        const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeRandom}`);
+        const data = await respuesta.json();
+        console.log(data);
+    } catch (error) {
+        console.log(TienesUnError);
+    }
+}
+aaa
